61485e7ec6080ce1e0a4ad177ad92f289c801177
Shoru <bapcui596@gmail.com>
2023-08-20 02:08:13 +0700

    dev: firestore extension init

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:petcare_search/appStyle.dart';
@@ -6,16 +7,23 @@ import 'package:petcare_search/screens/add_detail_pet.dart';
 import 'package:petcare_search/screens/appointmentScreen.dart';
 
 import 'package:petcare_search/screens/editPro5_Screen.dart';
+import 'package:petcare_search/screens/mainSearch.dart';
 
 import 'package:petcare_search/screens/profileScreen.dart';
 import 'package:petcare_search/screens/sign_in.dart';
 import 'package:petcare_search/screens/welcome.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
   ));
-
+  
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
@@ -29,7 +37,7 @@ class MyApp extends StatelessWidget {
       title: 'Balls',
       theme: AppTheme,
 
-      home: AddDetailPet(),
+      home: SearchMain(),
 
 
     );
