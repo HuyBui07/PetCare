f00be8d00aa46ad7e3d2d213beab6071a9b12280
Gia Huy <21520909@gm.uit.edu.vn>
2023-08-21 11:47:53 +0700

    ft: merge all branches

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,14 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:petcare_search/appStyle.dart';
-<<<<<<< HEAD
-import 'package:petcare_search/screens/registration.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-=======
-import 'package:petcare_search/screens/add_detail_pet.dart';
-
-import 'package:petcare_search/screens/appointmentScreen.dart';
-
-import 'package:petcare_search/screens/editPro5_Screen.dart';
-import 'package:petcare_search/screens/mainSearch.dart';
-import 'package:petcare_search/screens/petPro5_Screen.dart';
-
-import 'package:petcare_search/screens/profile_Screen.dart';
-import 'package:petcare_search/screens/sign_in.dart';
 import 'package:petcare_search/screens/welcome.dart';
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
   ));
 
->>>>>>> main
   runApp(const MyApp());
 }
 
@@ -39,11 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Balls',
       theme: AppTheme,
-<<<<<<< HEAD
-      home: const Registration(),
-=======
-      home: PetProfileScreen(),
->>>>>>> main
+      home: Welcome(),
     );
   }
 }
